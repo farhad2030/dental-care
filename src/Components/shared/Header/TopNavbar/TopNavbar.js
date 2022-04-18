19f4c7ded68de04b98c4e4c8be1bb43cc4d3c08a
Md Mazharul Islam Farhad <mazharul383all@gmail.com>
2022-04-18 06:10:57 +0600

    add sigin out

--- src/Components/shared/Header/TopNavbar/TopNavbar.js
+++ src/Components/shared/Header/TopNavbar/TopNavbar.js
@@ -1,31 +1,61 @@
 import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
-import {Link} from "react-router-dom"
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
+import { Link } from "react-router-dom";
+import auth from "../../../../firebase.init";
+
 const TopNavbar = () => {
+  const [user, loading, error] = useAuthState(auth);
+
   return (
-    <div>
-      <Nav
-        activeKey="/home"
-        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-      >
-        <Nav.Item>
-          <Nav.Link href="/home">Active</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">Link</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link as={Link} to="Login">
-            Login
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="disabled" disabled>
-            Disabled
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="container-fluid">
+        <Link class="navbar-brand" to="#">
+          Dental-Care
+        </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <Link class="nav-link " aria-current="page" to="/home#services">
+                services
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link " aria-current="page" to="/blog">
+                Blog
+              </Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" to="aboutMe">
+                About Me
+              </Link>
+            </li>
+            <li class="nav-item">
+              {user ? (
+                <Link class="nav-link" to="#!" onClick={() => signOut(auth)}>
+                  Signout
+                </Link>
+              ) : (
+                <Link class="nav-link" to="/login">
+                  Login
+                </Link>
+              )}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
